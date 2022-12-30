d59156f305fb76c54a1050e987d2d37de9d4b81c
Rafael Alberto Larios <albertolarios@me.com>
2022-12-30 13:16:04 -0600

    Implement LinkedList and Node factories

---
+++ linkedList.js
@@ -0,0 +1,14 @@
+const LinkedList = function() {
+    const head = {
+        next: null
+    }
+
+    return { head };
+};
+
+const Node = function(value) {
+    value: value,
+    next: null;
+};
+
+module.exports = { LinkedList };
