56250d605b8f3d89486f0157703dafbe2d859a42
Rafael Alberto Larios <albertolarios@me.com>
2022-12-30 14:35:31 -0600

    Add LinkedList methods, change paradigm and make head the very first element

--- linkedList.js
+++ linkedList.js
@@ -1,14 +1,62 @@
 const LinkedList = function() {
-    const head = {
-        next: null
+    let _head = null;
+
+    const head = function () {
+        return _head;
+    };
+
+    const prepend = function(value) {
+        const node = Node(value);
+        node.next = _head;
+        _head = node;
+        return node;
     }
 
-    return { head };
+    const append = function(value) {
+        const node = Node(value);
+        if (!_head) {
+            _head = node;
+            return node;
+        }
+        tail().next = node;
+        return node;
+    };
+
+    const tail = function() {
+        if (!_head) return _head;
+        let node = _head;
+        while (node.next) {
+            node = node.next;
+        }
+        return node;
+    };
+
+    const size = function() {
+        if (!_head) return 0;
+        let node = _head;
+        let count = 1;
+        while (node.next) {
+            node = node.next;
+            count++;
+        }
+        return count;
+    }
+
+    return {
+        head,
+        prepend,
+        append,
+        tail,
+        size,
+    };
 };
 
 const Node = function(value) {
-    value: value,
-    next: null;
+    const setValue = value || null;
+    return {
+        value: setValue,
+        next: null
+    }
 };
 
 module.exports = { LinkedList };
